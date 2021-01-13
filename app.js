1186d68f09c8e1ba2f704a186b85df57eb56de1a
Pritam Kumar <pritam8987@gmail.com>
2021-01-13 01:46:03 -0500

    New features addd

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
-const { captureStackTrace } = require("./utils/ExpressError");
+const { campgroundSchema } = require("./schemas");
 const app = express();
 mongoose.connect("mongodb://localhost:27017/funcamp", {
 	useCreateIndex: true,
@@ -27,11 +26,22 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const validateCampground = (req, res, next) => {
+	const { error } = campgroundSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((el) => el.message).join(",");
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
 app.get(
 	"/campgrounds",
+
 	catchAsync(async (req, res) => {
 		const campgrounds = await Campground.find({});
 		res.render("campgrounds/index", { campgrounds });
@@ -44,24 +54,11 @@ app.get("/campgrounds/new", (req, res) => {
 
 app.post(
 	"/campgrounds",
+	validateCampground,
 	catchAsync(async (req, res) => {
 		// if (!req.body.campground)
 		// 	throw new ExpressError("Invalid Campground input", 400);
-		const campgroundSchema = Joi.object({
-			campground: Joi.object({
-				title: Joi.string().required(),
-				price: Joi.number().required().min(0),
-				image: Joi.string().required(),
-				location: Joi.string().required(),
-				description: Joi.string().required(),
-			}).required(),
-		});
-		const { error } = campgroundSchema.validate(req.body);
-		if (error) {
-			const msg = error.details.map((el) => el.message).join(",");
-			throw new ExpressError(msg, 400);
-		}
-		console.log(result);
+
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
@@ -78,6 +75,7 @@ app.get(
 
 app.get(
 	"/campgrounds/:id/edit",
+	validateCampground,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		res.render("campgrounds/edit", { campground });
