338bb02fc415d431391441c795ca37804b439eb0
Pritam Kumar <pritam8987@gmail.com>
2021-01-14 18:49:46 -0500

    'added creating review in db'

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const { campgroundSchema } = require("./schemas");
 const app = express();
+const Review = require("./models/review");
+
 mongoose.connect("mongodb://localhost:27017/funcamp", {
 	useCreateIndex: true,
 	useCreateIndex: true,
@@ -99,6 +101,18 @@ app.delete(
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
 		res.redirect("/campgrounds");
+	}),
+);
+
+app.post(
+	"/campgrounds/:id/reviews",
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
 	}),
 );
 
