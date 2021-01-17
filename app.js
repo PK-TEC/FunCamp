87b9325da279f06477820a221b36ee5b93c2c3de
Pritam Kumar <pritam8987@gmail.com>
2021-01-17 00:13:10 -0500

    'added more feature to the routers'

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const session = require("express-session");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
-
+const flash = require("connect-flash");
 const app = express();
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -26,14 +27,31 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
 
+const sessionConfig = {
+	secret: "itismysecret",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+};
+app.use(session(sessionConfig));
+app.use(flash());
 app.get("/", (req, res) => {
 	res.render("home");
 });
+app.use((req, res, next) => {
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+});
+
+app.use("/campgrounds", campgrounds);
+app.use("/campgrounds/:id/reviews", reviews);
 
 app.all("*", (req, res, next) => {
 	next(new ExpressError("Page Not Found", 404));
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,7 +5,6 @@ const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const { campgroundSchema } = require("../schemas");
 
-
 const validateCampground = (req, res, next) => {
 	const { error } = campgroundSchema.validate(req.body);
 	if (error) {
@@ -31,9 +30,10 @@ router.get("/new", (req, res) => {
 router.post(
 	"/",
 	validateCampground,
-	catchAsync(async (req, res) => {
+	catchAsync(async (req, res, next) => {
 		const campground = new Campground(req.body.campground);
 		await campground.save();
+		req.flash("success", "Successfully made a new campground!");
 		res.redirect(`/campgrounds/${campground._id}`);
 	}),
 );
